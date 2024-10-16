bea40afa84a07e652df72442833421ed2df03024
John Mukhwana <Johnbradill67@gmail.com>
2024-10-16 18:00:28 +0300

    learning Variables

---
+++ Introduction/Variables/variables .cs
@@ -0,0 +1,6 @@
+string firstName;
+firstName = "Bob";
+int year = 5;
+string lastName = "Smith";
+float height = 5.5f;
+Console.WriteLine("Hello"+firstName+""+Latname+"You are"+year+"years old and"+height+"feet tall");
