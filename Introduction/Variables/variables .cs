47b19ebd183671808f1c39366e9e0ca3ce2ce7a5
John Mukhwana <Johnbradill67@gmail.com>
2024-10-16 18:21:42 +0300

    added character escape sequence

--- Introduction/Variables/variables .cs
+++ Introduction/Variables/variables .cs
@@ -1,6 +1,18 @@
-string firstName;
-firstName = "Bob";
-int year = 5;
-string lastName = "Smith";
-float height = 5.5f;
-Console.WriteLine("Hello"+firstName+""+Latname+"You are"+year+"years old and"+height+"feet tall");
+// string firstName;
+// firstName = "Bob";
+// int year = 5;
+// string lastName = "Smith";
+// float height = 5.5f;
+// Console.WriteLine("Hello"+firstName+""+Latname+"You are"+year+"years old and"+height+"feet tall");
+
+/*
+*Character sequence in C# is called a string. A string is a sequence of characters enclosed in double quotes.
+\ is an escape character. It is used to escape the character that follows it. For example, \n is a newline character.
+\t is a tab character. It is used to insert a tab in the output.
+*Variables are used to store data. A variable is a name given to a memory location in a program. The value stored in a variable can be changed during program execution.
+*/
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
