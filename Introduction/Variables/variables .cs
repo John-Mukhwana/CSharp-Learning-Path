fbe4e1d7dd7637828c8ed3d3316fb804a6aa9709
John Mukhwana <Johnbradill67@gmail.com>
2024-10-16 18:30:26 +0300

    added String interpolation

--- Introduction/Variables/variables .cs
+++ Introduction/Variables/variables .cs
@@ -12,7 +12,20 @@
 *Variables are used to store data. A variable is a name given to a memory location in a program. The value stored in a variable can be changed during program execution.
 */
 
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
+// Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+// Console.WriteLine("Invoice: 1021\t\tComplete!");
+// Console.WriteLine("Invoice: 1022\t\tComplete!");
+// Console.WriteLine("\nOutput Directory:\t");
+
+/*
+*String interpolation is a way to construct strings that include variables. It is a way to embed expressions inside string literals, using curly braces {}.
+*The $ symbol is used to indicate that the string is an interpolated string.
+*The curly braces {} are used to embed expressions inside the string.
+*The expression inside the curly braces can be a variable, a property, or a method call.
+*Example
+*/
+//example
+string naame="John";
+int age=30;
+float height=5.7f;
+Console.WriteLine($"Hello {name},you are {age} years old and {height} feet tall");
